39d8a22b82cbfdd18198be75ff3bdc66f407902c
sheep94lion <sheep94lion@gmail.com>
2015-12-20 16:54:17 +0800

    init

---
+++ BlinkToRadio/BlinkToRadio.h
@@ -0,0 +1,14 @@
+#ifndef BLINKTORADIO_H
+#define BLINKTORADIO_H
+
+enum{
+    MOTE_ID = 2,
+    AM_BLINKTORADIOMSG = 6,
+    TIMER_PERIOD_MILLLI = 250
+};
+
+typedef nx_struct BlinkToRadioMsg {
+    nx_uint16_t nodeid;
+    nx_uint16_t counter;
+} BlinkToRadioMsg;
+#endif
